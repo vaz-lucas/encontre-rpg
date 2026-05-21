d2eb2720cdcf6c958122cbba73975e99f3f2f184
vaz-lucas <lucas.vazxp@gmail.com>
2026-05-21 17:28:54 -0300

    formatação CSS zerada, pronto para Tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,14 @@
 import './App.css'
-import { useState } from 'react'
+import { Greet } from './components/Greet'
 
 function App() {
 
-function greet(person: string) {
-  console.log(`Hello, ${person}!`)
-  return `Hello, ${person}!`
-}
 
-const [user, setUser] = useState('User')
-
-function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-  setUser(event.target.value)
-}
 
   return (
     <>
-  <div>
-<h1>{greet('world')}</h1>
-<p>Iniciando o projeto. Primeiro commit incoming!</p>
-
-
-<h2>Hello, {user}!</h2> 
-
-<input type="text" value={user} onChange={handleInputChange} placeholder="Your name" />
+    <div>
+<Greet />
   </div>
    </>
   )
---
+++ client/src/components/Greet.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function Greet() {
+
+    function greet(person: string) {
+  console.log(`Hello, ${person}!`)
+  return `Hello, ${person}!`
+}
+
+const [user, setUser] = useState('User')
+
+function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+  setUser(event.target.value)
+}
+
+return (
+    <>
+  <div>
+<h1>{greet('world')}</h1>
+<p>Iniciando o projeto. Primeiro commit incoming!</p>
+
+
+<h2>Hello, {user}!</h2> 
+
+<input type="text" value={user} onChange={handleInputChange} placeholder="Your name" />
+  </div>
+   </>
+  )
+
+}
